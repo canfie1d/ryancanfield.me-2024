d30fc0ade0075f6087e0678f43ff1cebec10c1e8
Ryan Canfield <ryancanfield@me.com>
2023-12-28 22:12:36 -0800

    Fix form

--- src/content/ContactContent.tsx
+++ src/content/ContactContent.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import styles from "../styles/content.module.scss";
 import formStyles from "../styles/form.module.scss";
 
+type FormData = {
+  "form-name": string;
+  name: string;
+  email: string;
+  message: string;
+  [key: string]: string; // Add index signature
+};
+
+const encode = (data: FormData) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const ContactContent = () => {
+  const [formData, setFormData] = useState<FormData>({
+    "form-name": "contact",
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = () => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ ...formData }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+  };
+
+  const handleChange = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className={styles.contentBody}>
       <p className={styles.p}>
@@ -9,15 +47,28 @@ const ContactContent = () => {
         familiar) people!
       </p>
       <form
-        className={formStyles.form}
         name="contact"
-        method="POST"
-        data-netlify="true"
+        data-netlify={true}
+        netlify-honeypot="bot-field"
+        hidden
       >
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+      <form className={formStyles.form} onSubmit={handleSubmit}>
+        <input type="hidden" name="contact" value="contact" />
         <label className={formStyles.label} htmlFor="name">
           Name
         </label>
-        <input className={formStyles.input} id="name" type="text" name="name" />
+        <input
+          className={formStyles.input}
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
         <label className={formStyles.label} htmlFor="email">
           Email Address
         </label>
@@ -26,6 +77,8 @@ const ContactContent = () => {
           className={formStyles.input}
           type="email"
           name="email"
+          onChange={handleChange}
+          value={formData.email}
         />
         <label className={formStyles.label} htmlFor="message">
           Message
@@ -34,12 +87,10 @@ const ContactContent = () => {
           id="message"
           className={formStyles.textarea}
           name="message"
+          onChange={handleChange}
+          value={formData.message}
         ></textarea>
-        <button
-          className={formStyles.button}
-          style={{ display: "block", margin: "0 auto 1rem" }}
-          type="submit"
-        >
+        <button className={formStyles.button} type="submit">
           Send
         </button>
       </form>
