3f05da49d24ae992879b529678dd11c9d38e9599
Ryan Canfield <ryancanfield@me.com>
2024-01-07 13:54:05 -0800

    Add Netllfy fn

--- src/components/ThemeMenu.tsx
+++ src/components/ThemeMenu.tsx
@@ -1,16 +1,29 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 import classNames from "classnames";
 import { useThemeContext } from "../contexts/ThemeProvider";
 import { ThemeTypes, themeConfig } from "../contexts/themeConfig";
 import Adjustments from "../icons/adjustments.svg?react";
 import styles from "../styles/themes.module.scss";
+import { createPortal } from "react-dom";
 
 const ThemeMenu = () => {
   const { theme, setTheme } = useThemeContext();
+  const [ghData, setGhData] = useState<any>(null);
 
   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
     const themeId = e.currentTarget.id as ThemeTypes;
     setTheme(themeId);
+  };
+
+  const handleBonusClick = async () => {
+    try {
+      const response = await fetch("/api/gh-contributions");
+
+      const data = await response.json();
+      setGhData(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   const renderThemeOptions = () => {
@@ -36,8 +49,41 @@ const ThemeMenu = () => {
 
   return (
     <footer className={classNames(styles.themeMenu)}>
+      {ghData &&
+        createPortal(
+          <div
+            style={{
+              position: "fixed",
+              inset: 0,
+              background: "#fff",
+              overflow: "auto",
+              padding: "1rem",
+            }}
+          >
+            <button
+              style={{ position: "sticky", top: 0 }}
+              onClick={() => setGhData(null)}
+            >
+              X
+            </button>
+            <p>{JSON.stringify(ghData)}</p>
+          </div>,
+          document.body,
+          "gh-data"
+        )}
       <span className={styles.themeMenuHeader}>
-        <Adjustments /> <span>Theme</span>
+        <button
+          style={{
+            background: "none",
+            padding: 0,
+            margin: 0,
+            border: 0,
+          }}
+          onClick={handleBonusClick}
+        >
+          <Adjustments />
+        </button>{" "}
+        <span>Theme</span>
       </span>
       <ul>{renderThemeOptions()}</ul>
     </footer>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,29 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import Sitemap from "vite-plugin-sitemap";
 import { VitePWA } from "vite-plugin-pwa";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), Sitemap(), VitePWA(), mkcert()],
+  plugins: [react(), svgr(), Sitemap(), VitePWA() /*mkcert()*/],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+    },
+  },
   server: {
     port: 3000,
+    // https: {
+    //   key: "./net-fn-key.pem",
+    //   cert: "./net-fn.pem",
+    // },
+    // open: true,
+    // proxy: {
+    //   "/api": {
+    //     target: "https://localhost:8888/functions",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
 });
