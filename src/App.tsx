9223090d000ecc4ac159f1cda892b11166212861
Ryan Canfield <ryancanfield@me.com>
2024-01-06 22:30:38 -0800

    App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Pages from "./pages/Pages";
 import Layout from "./components/Layout";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/useThemeProvider";
-import "./styles/globals.scss";
+import { ThemeProvider } from "./contexts/ThemeProvider";
+import { PageScrollProvider } from "./contexts/PageScrollProvider";
 import React from "react";
 import ReactDOM from "react-dom";
 import PageTitle from "./components/PageTitle";
 import ThemeMenu from "./components/ThemeMenu";
+import "./styles/globals.scss";
 
 if (process.env.NODE_ENV !== "production") {
   let { default: axe } = await import("@axe-core/react");
@@ -18,13 +19,15 @@ const App = () => {
   return (
     <ErrorBoundary>
       <ThemeProvider>
-        <Layout>
-          <Router>
-            <Pages />
-            <PageTitle />
-            <ThemeMenu />
-          </Router>
-        </Layout>
+        <PageScrollProvider>
+          <Layout>
+            <Router>
+              <Pages />
+              <PageTitle />
+              <ThemeMenu />
+            </Router>
+          </Layout>
+        </PageScrollProvider>
       </ThemeProvider>
     </ErrorBoundary>
   );
