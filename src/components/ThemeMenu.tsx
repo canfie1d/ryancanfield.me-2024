0d905d7dc592486360622a89436f25452cacbc7b
Ryan Canfield <ryancanfield@me.com>
2024-01-07 14:05:46 -0800

    Adds GH contributions UI

--- src/components/ThemeMenu.tsx
+++ src/components/ThemeMenu.tsx
@@ -1,15 +1,27 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import { useThemeContext } from "../contexts/ThemeProvider";
 import { ThemeTypes, themeConfig } from "../contexts/themeConfig";
 import Adjustments from "../icons/adjustments.svg?react";
 import styles from "../styles/themes.module.scss";
 import { createPortal } from "react-dom";
+import { drawContributions } from "github-contributions-canvas";
 
 const ThemeMenu = () => {
+  const canvasRef = useRef(null);
   const { theme, setTheme } = useThemeContext();
   const [ghData, setGhData] = useState<any>(null);
 
+  useEffect(() => {
+    if (canvasRef.current && ghData) {
+      drawContributions(canvasRef.current, {
+        data: ghData.data,
+        username: "canfie1d",
+        themeName: "standard",
+        fontFace: "Nunito",
+      });
+    }
+  }, [ghData]);
   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
     const themeId = e.currentTarget.id as ThemeTypes;
     setTheme(themeId);
@@ -66,7 +78,7 @@ const ThemeMenu = () => {
             >
               X
             </button>
-            <p>{JSON.stringify(ghData)}</p>
+            <canvas ref={canvasRef} style={{ maxWidth: "100%" }} />
           </div>,
           document.body,
           "gh-data"
